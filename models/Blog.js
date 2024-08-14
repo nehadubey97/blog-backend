36c366787081f5d5cbd9ac5b2284acaf5b863982
Neha Dubey <snehadubey2697@gmail.com>
2024-08-14 22:16:29 +0530

    added blog schema

--- models/Blog.js
+++ models/Blog.js
@@ -1,1 +1,25 @@
 const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Blog = mongoose.model('Blog', blogSchema)
+module.exports = Blog
